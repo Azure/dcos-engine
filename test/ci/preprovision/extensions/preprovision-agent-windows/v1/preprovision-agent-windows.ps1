9201e4ff9ea8952206c7471c89ca8fd3e13a9237
dmitsh <dmitsh@microsoft.com>
2018-09-12 22:09:36 -0700

    update Windows provision test scripts and configs (#56)

--- test/ci/preprovision/extensions/preprovision-agent-windows/v1/preprovision-agent-windows.ps1
+++ test/ci/preprovision/extensions/preprovision-agent-windows/v1/preprovision-agent-windows.ps1
@@ -1,8 +1,6 @@
 $ErrorActionPreference = "Stop"
 
 $MESOS_ETC_SERVICE_DIR = Join-Path $env:SystemDrive "DCOS-etc\mesos\service"
-$MESOS_SERVICE_NAME = "dcos-mesos-slave"
-
 
 filter Timestamp { "[$(Get-Date -Format o)] $_" }
 
@@ -12,95 +10,6 @@ function Write-Log {
     )
     $msg = $message | Timestamp
     Write-Output $msg
-}
-
-function Start-ExecuteWithRetry {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [ScriptBlock]$ScriptBlock,
-        [int]$MaxRetryCount=10,
-        [int]$RetryInterval=3,
-        [string]$RetryMessage,
-        [array]$ArgumentList=@()
-    )
-    $currentErrorActionPreference = $ErrorActionPreference
-    $ErrorActionPreference = "Continue"
-    $retryCount = 0
-    while ($true) {
-        Write-Log "Start-ExecuteWithRetry attempt $retryCount"
-        try {
-            $res = Invoke-Command -ScriptBlock $ScriptBlock `
-                                  -ArgumentList $ArgumentList
-            $ErrorActionPreference = $currentErrorActionPreference
-            Write-Log "Start-ExecuteWithRetry terminated"
-            return $res
-        } catch [System.Exception] {
-            $retryCount++
-            if ($retryCount -gt $MaxRetryCount) {
-                $ErrorActionPreference = $currentErrorActionPreference
-                Write-Log "Start-ExecuteWithRetry exception thrown"
-                throw
-            } else {
-                if($RetryMessage) {
-                    Write-Log "Start-ExecuteWithRetry RetryMessage: $RetryMessage"
-                } elseif($_) {
-                    Write-Log "Start-ExecuteWithRetry Retry: $_.ToString()"
-                }
-                Start-Sleep $RetryInterval
-            }
-        }
-    }
-}
-
-function Start-FileDownloadWithCurl {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [string]$URL,
-        [Parameter(Mandatory=$true)]
-        [string]$Destination,
-        [Parameter(Mandatory=$false)]
-        [int]$RetryCount=10
-    )
-    $params = @('-fLsS', '-o', "`"${Destination}`"", "`"${URL}`"")
-    Start-ExecuteWithRetry -ScriptBlock {
-        $p = Start-Process -FilePath 'curl.exe' -NoNewWindow -ArgumentList $params -Wait -PassThru
-        if($p.ExitCode -ne 0) {
-            Throw "Fail to download $URL"
-        }
-    } -MaxRetryCount $RetryCount -RetryInterval 3 -RetryMessage "Failed to download ${URL}. Retrying"
-}
-
-function Start-CIAgentSetup {
-    # Pre-pull the IIS image
-    Start-ExecuteWithRetry -ScriptBlock { docker.exe pull "microsoft/iis:windowsservercore-1803" } `
-                           -MaxRetryCount 30 -RetryInterval 3 -RetryMessage "Failed to pull IIS image. Retrying"
-    # Enable Docker debug logging and capture stdout and stderr to a file.
-    # We're using the updated service wrapper for this.
-    $serviceName = "Docker"
-    $dockerHome = Join-Path $env:ProgramFiles "Docker"
-    $wrapperUrl = "http://dcos-win.westus2.cloudapp.azure.com/downloads/service-wrapper.exe"
-    Stop-Service $serviceName
-    sc.exe delete $serviceName
-    if($LASTEXITCODE) {
-        Throw "Failed to delete service: $serviceName"
-    }
-    Start-FileDownloadWithCurl -URL $wrapperUrl -Destination "${dockerHome}\service-wrapper.exe" -RetryCount 30
-    $binPath = ("`"${dockerHome}\service-wrapper.exe`" " +
-                "--service-name `"$serviceName`" " +
-                "--exec-start-pre `"powershell.exe if(Test-Path '${env:ProgramData}\docker\docker.pid') { Remove-Item -Force '${env:ProgramData}\docker\docker.pid' }`" " +
-                "--log-file `"$dockerHome\dockerd.log`" " +
-                "`"$dockerHome\dockerd.exe`" -D")
-    New-Service -Name $serviceName -StartupType "Automatic" -Confirm:$false `
-                -DisplayName "Docker Windows Agent" -BinaryPathName $binPath
-    sc.exe failure $serviceName reset=5 actions=restart/1000
-    if($LASTEXITCODE) {
-        Throw "Failed to set $serviceName service recovery options"
-    }
-    sc.exe failureflag $serviceName 1
-    if($LASTEXITCODE) {
-        Throw "Failed to set $serviceName service recovery options"
-    }
-    Start-Service $serviceName
 }
 
 function Write-MesosSecretFiles {
@@ -127,18 +36,22 @@ function Write-MesosSecretFiles {
     }
     $json = ConvertTo-Json -InputObject $httpCredentials -Compress
     [System.IO.File]::WriteAllLines("$MESOS_ETC_SERVICE_DIR\http_credential.json", $json, $utf8NoBOM)
+
     # Create the Mesos service environment file with authentication enabled
     $serviceEnv = @(
-        "MESOS_AUTHENTICATE_HTTP_READONLY=true",
-        "MESOS_AUTHENTICATE_HTTP_READWRITE=true",
-        "MESOS_HTTP_CREDENTIALS=$MESOS_ETC_SERVICE_DIR\http_credential.json",
-        "MESOS_CREDENTIAL=$MESOS_ETC_SERVICE_DIR\credential.json"
+        "`$env:MESOS_AUTHENTICATE_HTTP_READONLY='true'",
+        "`$env:MESOS_AUTHENTICATE_HTTP_READONLY='true'",
+        "`$env:MESOS_AUTHENTICATE_HTTP_READWRITE='true'",
+        "`$env:MESOS_HTTP_CREDENTIALS=`"$MESOS_ETC_SERVICE_DIR\http_credential.json`"",
+        "`$env:MESOS_CREDENTIAL=`"$MESOS_ETC_SERVICE_DIR\credential.json`""
     )
-    Set-Content -Path "${MESOS_ETC_SERVICE_DIR}\environment-file" -Value $serviceEnv -Encoding utf8
+
+    New-Item -ItemType "Directory" -Path "C:\azuredata\mesos" -Force
+    $AuthEnvFile = "C:\azuredata\mesos\auth-env.ps1"
+    Set-Content -Path $AuthEnvFile -Value $serviceEnv -Encoding utf8
 }
 
 try {
-    Start-CIAgentSetup
     Write-MesosSecretFiles
     Write-Output "Successfully executed the preprovision-agent-windows.ps1 script"
 } catch {
